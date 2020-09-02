03a961d374dbbfca42191836af7491cdf221df41
Suravarus <57523665+Suravarus@users.noreply.github.com>
2020-09-02 02:28:08 -0500

    Add basic project
    Added player movement and camera following the player. Assets are placeholders.

---
+++ Bullet Hell/Assets/CameraFollow.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections.Specialized;
+using System.Security.Cryptography;
+using UnityEngine;
+
+public class CameraFollow : MonoBehaviour
+{
+    public Transform target;
+        
+    //LateUpdates occurrs directly after Update
+    void LateUpdate()
+    {
+        transform.position = new Vector3(target.position.x, target.position.y, transform.position.z);
+    }
+
+
+}
---
+++ Bullet Hell/Assets/PlayerMovement.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Threading;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+
+    public float moveSpeed = 5f;
+
+    public Rigidbody2D rb;
+
+    Vector2 movement;
+
+    //inputs are taken once per frame
+    void Update()
+    {
+        float x = Input.GetAxisRaw("Horizontal");
+        float y = Input.GetAxisRaw("Vertical");
+        //slow down if neither are 0, sqrt2 movement in both directions. 
+        //0.70710678118 is sqrt(2) / 2
+        if (x != 0 && y != 0)
+        {
+            movement.x = x * 0.70710678118f;
+            movement.y = y * 0.70710678118f;
+        }
+        else
+        {
+            movement.x = x;
+            movement.y = y;
+        }
+    }
+
+    //called on a timer, not tied to framerate. Called 50 times per second by default.
+    void FixedUpdate()
+    {
+        rb.MovePosition(rb.position + movement * moveSpeed * Time.fixedDeltaTime);
+    }
+}
