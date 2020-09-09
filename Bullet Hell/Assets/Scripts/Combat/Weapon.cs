15b7b76c1a956b18886f2aa203bc632763098d07
Umair Arshad <uarshad134@gmail.com>
2020-09-08 21:48:45 -0500

    Outline of Bullet and Enemy

---
+++ Bullet Hell/Assets/Scripts/Combat/Weapon.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Weapon : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        Weapon_speed = 1;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //on mouse click
+
+
+        //spawn bullet, set dir, set speed
+
+
+    }
+}
