35d6070a162c71991aab46776ad1773e0239e0d8
Umair Arshad <uarshad134@gmail.com>
2020-09-12 16:42:07 -0500

    Add Bow plus shooting
    Added bow and fire with space in the Dev_combat scenario

--- Bullet Hell/Assets/Scripts/Combat/Weapon.cs
+++ Bullet Hell/Assets/Scripts/Combat/Weapon.cs
@@ -1,144 +1,134 @@
-<<<<<<< HEAD
-﻿using System.Collections;
+using Combat;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using System;
 
 public class Weapon : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    [Header("Ammo")]
+    public GameObject cannonAmmo;
+    public int baseDamage;
+    public float bulletSpeed;
+    [Header("Combat")]
+    [Tooltip("Bullets per second")]
+    public float rateOfFire = 1f;
+    private float fireDelay;
+    // will keep track of the last time this weapon 'fired'
+    // private float LastFiredDeltaTime { get; set; }
+    // private float TimeOfFireRequest { get; set; }
+    private float TimeSinceFireRequest { get; set; }
+    // private float ScheduledTimeOfFire { get; set; }
+    private bool WaitingToFire { get; set; }
+    private float FireDelay { get; set; }
+    public GameObject CannonAmmo
     {
-        Weapon_speed = 1;
+        set { this.cannonAmmo = value; }
+        get { return this.cannonAmmo; }
     }
+
+    public float RateOfFire
+    {
+        set { this.rateOfFire = value; }
+        get { return this.rateOfFire; }
+    }
+    
 
     // Update is called once per frame
     void Update()
     {
-        //on mouse click
-
-
-        //spawn bullet, set dir, set speed
+        //point towards mouse rotating around player
+        Vector3 difference = Camera.main.ScreenToWorldPoint(Input.mousePosition) - transform.position;
+        difference.Normalize();
+        float rotationZ = Mathf.Atan2(difference.y, difference.x) * Mathf.Rad2Deg - 90;
+        transform.rotation = Quaternion.Euler(0f, 0f, rotationZ);
 
 
     }
-=======
-﻿using System;
-using UnityEngine;
 
-namespace Combat
-{
-    public class Weapon : MonoBehaviour
+    
+    void Awake()
     {
-        [Header("Ammo")]
-        public GameObject cannonAmmo;
-        public int baseDamage;
-        public float bulletSpeed;
-        [Header("Combat")]
-        [Tooltip("Bullets per second")]
-        public float rateOfFire = 1f;
-        private float fireDelay;
-        // will keep track of the last time this weapon 'fired'
-        // private float LastFiredDeltaTime { get; set; }
-        // private float TimeOfFireRequest { get; set; }
-        private float TimeSinceFireRequest { get; set; }
-        // private float ScheduledTimeOfFire { get; set; }
-        private bool WaitingToFire { get; set; }
-        private float FireDelay { get; set; }
-        public GameObject CannonAmmo 
-        { 
-            set { this.cannonAmmo = value; }    
-            get { return this.cannonAmmo; }
-        }
+        // check that fire rate has not been set to negative.
+        if (this.rateOfFire < 0)
+            throw new ArgumentOutOfRangeException(
+                "Cannon.Awake() - Rate of Fire cannot be < 0."
+            );
+        // calculate FireDelay based on rateOfFire
+        this.FireDelay = (1f / this.rateOfFire);
+        // set initial parameters.
+        //this.LastFiredDeltaTime = 0f;
+        //this.TimeOfFireRequest = 0f;
+        this.TimeSinceFireRequest = 0f;
+        //this.ScheduledTimeOfFire = 0f;
+        this.WaitingToFire = false;
+    }
 
-        public float RateOfFire
+    void FixedUpdate()
+    {
+        // Check if spacebar has is being pressed
+        if (Input.GetKey(KeyCode.Space) == true)
         {
-            set { this.rateOfFire = value; }
-            get { return this.rateOfFire; }
+            this.RequestCannonFire();
         }
-
-        void Awake()
+        // update elapsed time
+        this.TimeSinceFireRequest += Time.deltaTime;
+        // check if cannon is waiting to fire
+        if (this.WaitingToFire)
         {
-            // check that fire rate has not been set to negative.
-            if (this.rateOfFire < 0)
-                throw new ArgumentOutOfRangeException(
-                    "Cannon.Awake() - Rate of Fire cannot be < 0."
-                );
-            // calculate FireDelay based on rateOfFire
-            this.FireDelay = (1f / this.rateOfFire);
-            // set initial parameters.
-            //this.LastFiredDeltaTime = 0f;
-            //this.TimeOfFireRequest = 0f;
-            this.TimeSinceFireRequest = 0f;
-            //this.ScheduledTimeOfFire = 0f;
-            this.WaitingToFire = false;
-        }
-
-        void FixedUpdate()
-        {
-            // Check if spacebar has is being pressed
-            if (Input.GetKey(KeyCode.Space) == true)
+            // check if current time == the requested time of fire.
+            if (this.TimeSinceFireRequest >= this.FireDelay)
             {
-                this.RequestCannonFire();
-            }
-            // update elapsed time
-            this.TimeSinceFireRequest += Time.deltaTime;
-            // check if cannon is waiting to fire
-            if (this.WaitingToFire)
-            {
-                // check if current time == the requested time of fire.
-                if (this.TimeSinceFireRequest >= this.FireDelay)
-                {
-                    // fire cannon
-                    this.Fire();
-                    // reset wait parameters
-                    this.WaitingToFire = false;
-                    //this.ScheduledTimeOfFire = 0f;
-                    //this.TimeOfFireRequest = 0f;
-
-                }
-            }
-        }
-
-        public void RequestCannonFire()
-        {
-            if (this.FireDelay == 0)
-            {
+                // fire cannon
                 this.Fire();
-            }
-            else if (!this.WaitingToFire)
-            {
-                // set the neccessary parameters for a delayed 'fire'
-                this.TimeSinceFireRequest = 0f;
-                // this.ScheduledTimeOfFire = this.TimeOfFireRequest + this.FireDelay;
-                this.WaitingToFire = true;
-            }
-        }
+                // reset wait parameters
+                this.WaitingToFire = false;
+                //this.ScheduledTimeOfFire = 0f;
+                //this.TimeOfFireRequest = 0f;
 
-        /// <summary> fire cannon.</summary>
-        /// <exception> Is Ammo NULL? </exception>
-        private void Fire()
-        {
-            try
-            {
-                
-                var a = this.CannonAmmo.GetComponent<Ammo>();
-                // shoot the 'ammo' straight ahead
-                if (this.bulletSpeed > 0)
-                    a.speed = this.bulletSpeed;
-                if (this.baseDamage > 0)
-                    a.baseDamage = this.baseDamage;
-
-                Instantiate(this.CannonAmmo,
-                this.transform.position,
-                this.transform.rotation);
-            }
-            catch (Exception ex)
-            {
-                throw new Exception(
-                    "Weapon.Fire() - has Ammo been set?", ex);
             }
         }
     }
 
->>>>>>> 78bcc15652e0727f8e7f76a55a18eac81917253b
+    public void RequestCannonFire()
+    {
+        if (this.FireDelay == 0)
+        {
+            this.Fire();
+        }
+        else if (!this.WaitingToFire)
+        {
+            // set the neccessary parameters for a delayed 'fire'
+            this.TimeSinceFireRequest = 0f;
+            // this.ScheduledTimeOfFire = this.TimeOfFireRequest + this.FireDelay;
+            this.WaitingToFire = true;
+        }
+    }
+
+    /// <summary> fire cannon.</summary>
+    /// <exception> Is Ammo NULL? </exception>
+    private void Fire()
+    {
+        try
+        {
+
+            var a = this.CannonAmmo.GetComponent<Ammo>();
+            // shoot the 'ammo' straight ahead
+            if (this.bulletSpeed > 0)
+                a.speed = this.bulletSpeed;
+            if (this.baseDamage > 0)
+                a.baseDamage = this.baseDamage;
+
+            Instantiate(this.CannonAmmo,
+            this.transform.position,
+            this.transform.rotation);
+        }
+
+        catch (Exception ex)
+        {
+            throw new Exception(
+                "Weapon.Fire() - has Ammo been set?", ex);
+        }
+    }
+    
 }
