4c6a682d6eb0baf1cb03df6223721fa9c254cdd1
chvz-creates <17281817+chvz-creates@users.noreply.github.com>
2020-09-29 02:24:45 -0500

    updated ammo
    - does not destroy on colliding with player
    - added weapon property.
    - Destroys itself if it travels beyond the specified weapon.range.
    - added property for ammoOwner so that it can detect collision with its owner.

--- Bullet Hell/Assets/Scripts/Combat/Ammo.cs
+++ Bullet Hell/Assets/Scripts/Combat/Ammo.cs
@@ -1,4 +1,5 @@
-﻿using UnityEngine;
+﻿using System;
+using UnityEngine;
 namespace Combat
 {
     public class Ammo : MonoBehaviour
@@ -9,35 +10,71 @@ namespace Combat
         [Header("Combat")]
         public int baseDamage = 1;
 
-        public Rigidbody2D rb;
+        public Combatant ammoOwner;
+        public Weapon weapon;
 
-        ///// <summary>Sent when another object enters a trigger 
-        /////     collider attached to this object (2D physics only).</summary>
-        ///// See: https://docs.unity3d.com/ScriptReference/Collider2D.OnTriggerEnter2D.html
-        void OnTriggerEnter2D(Collider2D collisionInfo)
-        {
-            print("Hit: " + collisionInfo.name);
+        private Rigidbody2D rigidBody2D;
+        private Vector2 startingPosition;
+        
 
-            var enemy = collisionInfo.GetComponent<Combatant>();
-            
-            if (enemy != null)
-            {
-                enemy.TakeDamage(this.baseDamage);
-                print("DEBUG: Asteroid health: " + enemy._health);
-            }
-
-            Destroy(this.gameObject);
-        }
-
+        // ALGORITHM:
+        //    IF the rgb2d component exists:
+        //        POINT to component
+        //    ELSE:
+        //        THROW error
         private void Awake()
         {
-            this.rb = this.GetComponent<Rigidbody2D>();
+            // IF has rgb2d component -> SET rgb2d
+            // ELSE -> throw error
+            if (this.GetComponent<Rigidbody2D>() != null)
+            {
+                this.rigidBody2D = this.GetComponent<Rigidbody2D>();
+            } else
+            {
+                throw new MissingComponentException(
+                    $"Missing component: {new Rigidbody2D().GetType().Name}");
+            }
+
+            //var spawnPoint = this.GetComponentInParent<Transform>();
+            //var weapon = spawnPoint.GetComponentInParent<Weapon>();
+            //this.ammoOwner = weapon.GetComponentInParent<Combatant>();
+            Debug.Log($"Ammo owner is {this.ammoOwner.name}");
+   
         }
 
+        // ALGORITHM:
+        //     SET rgb2d velocity
         void Start()
         {
-            //this.tag = "laser";
-            rb.velocity = transform.up * speed;
+            this.startingPosition = this.transform.position;
+            this.rigidBody2D.velocity = this.transform.up * speed;
+        }
+
+        private void FixedUpdate()
+        {
+            if (Vector2.SqrMagnitude(new Vector2(this.transform.position.x, this.transform.position.y) - this.startingPosition) > this.weapon.range)
+            {
+                Destroy(this.gameObject);
+            }
+        }
+
+        // ALGORITHM:
+        //     PRINT Debug collision info
+        //     GET enemy combatant from the collisionInfo
+        //     IF colliding object is an enemy:
+        //         
+        void OnTriggerEnter2D(Collider2D collisionInfo)
+        {
+            Debug.Log($"{this.GetType().Name} - {this.gameObject.name} collided with {collisionInfo.name}");
+
+            var enemy = collisionInfo.GetComponent<Combatant>();
+
+            if (enemy != null && enemy.name != this.ammoOwner.name)
+            {
+                enemy.TakeDamage(this.baseDamage);
+                Debug.Log($"{this.GetType().Name}: enemy health is " + enemy._health);
+                Destroy(this.gameObject);
+            }
         }
     }
 }
