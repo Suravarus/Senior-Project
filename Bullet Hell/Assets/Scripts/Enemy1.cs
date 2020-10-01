a2fb0b1990c53aa376a9e3b17168ed503f074594
chvz-creates <17281817+chvz-creates@users.noreply.github.com>
2020-10-01 18:32:52 -0500

    throws err if no rigidbody component
    - checks for rigidbody component on Awake() instead of Start()
    - throws custom error if no rigidbody component is found

--- Bullet Hell/Assets/Scripts/Enemy1.cs
+++ Bullet Hell/Assets/Scripts/Enemy1.cs
@@ -10,9 +10,21 @@ public class Enemy1 : MonoBehaviour
     private Rigidbody2D enemyRB; //the unit which has the script attached
     private Vector2 lookDirection; //enemy vision direction
 
-    private void Start()
+    private void Awake()
     {
-        enemyRB = GetComponent<Rigidbody2D>();
+        // CHECK for Rigidbody Component -----------------
+        var rb = this.GetComponent<Rigidbody2D>();
+        if (rb != null)
+        {
+            // SET enemyRB
+            this.enemyRB = rb;
+            
+        }
+        else // IF Rigidbody component does not exist -> THROW ERR
+        {
+            var err = new MissingComponentException($"Missing component: {new Rigidbody2D().GetType().Name}");
+            throw err;
+        }
     }
 
     // Update is called once per frame
