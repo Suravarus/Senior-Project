bdf1ae6623b0bf4048537bd852ef53a5a1a43fa0
chvz-creates <17281817+chvz-creates@users.noreply.github.com>
2020-10-02 01:10:49 -0500

    Update Ammo.cs
    - startingPosition is now a 3D Vector
    - Destroyed if collides with any
      trigger-collider that is not the player

--- Bullet Hell/Assets/Scripts/Combat/Ammo.cs
+++ Bullet Hell/Assets/Scripts/Combat/Ammo.cs
@@ -14,7 +14,7 @@ namespace Combat
         public Weapon weapon;
 
         private Rigidbody2D rigidBody2D;
-        private Vector2 startingPosition;
+        private Vector3 startingPosition;
         
 
         // ALGORITHM:
@@ -35,44 +35,61 @@ namespace Combat
                     $"Missing component: {new Rigidbody2D().GetType().Name}");
             }
 
-            //var spawnPoint = this.GetComponentInParent<Transform>();
-            //var weapon = spawnPoint.GetComponentInParent<Weapon>();
-            //this.ammoOwner = weapon.GetComponentInParent<Combatant>();
+            // TODO COMBAT-TEAM[2] check that all properties have been set.
             Debug.Log($"Ammo owner is {this.ammoOwner.name}");
-   
         }
 
         // ALGORITHM:
-        //     SET rgb2d velocity
+        // - SET startingPosition
+        // - SET velocity
         void Start()
         {
             this.startingPosition = this.transform.position;
             this.rigidBody2D.velocity = this.transform.up * speed;
         }
 
+        // ALGORITHM:
+        // - IF this gameObject has travelled beyond the range of the weapon that fired it:
+        // -   DESTROY this gameObject.
         private void FixedUpdate()
         {
-            if (Vector2.SqrMagnitude(new Vector2(this.transform.position.x, this.transform.position.y) - this.startingPosition) > this.weapon.range)
+            if ((this.transform.position - this.startingPosition).magnitude > this.weapon.range)
             {
                 Destroy(this.gameObject);
             }
         }
 
+        // FIXME COLLISION-TEAM[1] This should be on the Combatant script.
+        // PURPOSE:
+        // - Since this method is called whenever this gameObject collides with ANY
+        // - Collider2D object that is set to Collider.IsTrigger = True, this code
+        // - is being used to apply damage to any enemy this object collides with.
         // ALGORITHM:
-        //     PRINT Debug collision info
-        //     GET enemy combatant from the collisionInfo
-        //     IF colliding object is an enemy:
-        //         
+        // - PRINT Debug collision info
+        // - GET enemy combatant from the collisionInfo
+        // - IF collided with enemy<Combatabt> that does not own this ammo:
+        //     DO damage to enemy
+        //     DESTROY this gameObject
+        // - ELSE:
+        // -   DESTROY this gameObject
         void OnTriggerEnter2D(Collider2D collisionInfo)
         {
+            // PRINT Debug collision info
             Debug.Log($"{this.GetType().Name} - {this.gameObject.name} collided with {collisionInfo.name}");
-
+            // GET enemy combatant from the collisionInfo
             var enemy = collisionInfo.GetComponent<Combatant>();
-
-            if (enemy != null && enemy.name != this.ammoOwner.name)
+            // IF collided with enemy<Combatant> that does not own this ammo: 
+            if (enemy != null)
             {
-                enemy.TakeDamage(this.baseDamage);
-                Debug.Log($"{this.GetType().Name}: enemy health is " + enemy._health);
+                if(enemy.name != this.ammoOwner.name)
+                {
+                    // DO damage to enemy
+                    enemy.TakeDamage(this.baseDamage);
+                    Debug.Log($"{this.GetType().Name}: enemy health is " + enemy._health);
+                    Destroy(this.gameObject);
+                }
+            } else
+            {
                 Destroy(this.gameObject);
             }
         }
