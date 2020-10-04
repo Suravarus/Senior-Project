7f38f9b16b00bdaa08f654301a3b6862f0999f5e
HuyLe94 <70664981+HuyLe94@users.noreply.github.com>
2020-10-04 15:17:47 -0700

    TestFloor1
    modified camera and added camera bound range.

--- Bullet Hell/Assets/Scripts/CameraFollow.cs
+++ Bullet Hell/Assets/Scripts/CameraFollow.cs
@@ -4,12 +4,29 @@ using UnityEngine;
 
 public class CameraFollow : MonoBehaviour
 {
+    
     public Transform target;
-        
+    //[SerializeField]
+    //public float leftLimit;
+    //[SerializeField]
+    //public float rightLimit;
+    //[SerializeField]
+    //public float topLimit;
+    //[SerializeField]
+    //public float bottomLimit;
+
     //LateUpdates occurrs directly after Update
     void LateUpdate()
     {
         transform.position = new Vector3(target.position.x, target.position.y, transform.position.z);
+
+        ////Set bound
+        //transform.position = new Vector3
+        //    (
+        //        Mathf.Clamp(transform.position.x, leftLimit, rightLimit),
+        //        Mathf.Clamp(transform.position.x, bottomLimit, topLimit),
+        //        transform.position.z
+        //    );
     }
 
 
