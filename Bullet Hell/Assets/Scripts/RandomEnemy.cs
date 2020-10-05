c350f9d08a11aa011e81474d84a137148405dcd4
HuyLe94 <70664981+HuyLe94@users.noreply.github.com>
2020-10-04 18:42:35 -0700

    Random Enemy Spawn

---
+++ Bullet Hell/Assets/Scripts/RandomEnemy.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RandomEnemy : MonoBehaviour
+{
+    public Rigidbody2D[] enemy;
+    public int EnemyLimit;
+     int xPos;
+     int yPos;
+     int enemyCount;
+    public int xMapMin;
+    public int xMapMax;
+    public int yMapMin;
+    public int yMapMax;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        StartCoroutine(EnemyDrop());
+    }
+    IEnumerator EnemyDrop()
+    {
+        while (enemyCount < EnemyLimit)
+        {
+            xPos = Random.Range(xMapMin, xMapMax);
+            yPos = Random.Range(yMapMin, yMapMax);
+            Instantiate(enemy[Random.Range(0,2)], new Vector3(xPos, yPos, 0), Quaternion.identity);
+            yield return new WaitForSeconds(0.1f);
+            enemyCount += 1;
+        }
+    }
+
+
+}
