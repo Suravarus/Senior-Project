6b643dcae3ece1398be5b81ed3dab98f59af6455
HuyLe94 <70664981+HuyLe94@users.noreply.github.com>
2020-10-09 18:19:11 -0700

    new enemy prefab

---
+++ Bullet Hell/Assets/Scripts/Enemy3.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+
+public class Enemy3 : MonoBehaviour
+{
+    public Rigidbody2D playerRB;
+    private Rigidbody2D enemyRB;
+    private Vector2 lookDirection;
+    public float speed = 3f;
+    public float lowerRange = 3f;
+    public float upperRange = 5f;
+
+    private void Start()
+    {
+        enemyRB = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        lookDirection = playerRB.transform.position - transform.position;
+
+    }
+
+    private void FixedUpdate()
+    {
+        float magnitude = lookDirection.magnitude;
+        lookDirection = lookDirection.normalized;
+        if (magnitude < lowerRange)//move away from player
+        {
+            enemyRB.MovePosition(enemyRB.position + -lookDirection * speed * Time.fixedDeltaTime);
+        }
+        else if (magnitude > upperRange)//move towards player
+        {
+            enemyRB.MovePosition(enemyRB.position + lookDirection * speed * Time.fixedDeltaTime);
+        }
+
+        //no movement otherwise
+
+    }
+}
