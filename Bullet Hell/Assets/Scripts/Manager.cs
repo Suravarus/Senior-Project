3be305e4d9a065c0daa885fe734eb197cef1e7aa
Suravarus <57523665+Suravarus@users.noreply.github.com>
2020-10-10 18:20:23 -0500

    Custom Enemy pathfinding scripts
    Enemy chaser and shooter movement scripts added to treant and mole respectively.

---
+++ Bullet Hell/Assets/Scripts/Manager.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Manager : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public Rigidbody2D player;
+
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
