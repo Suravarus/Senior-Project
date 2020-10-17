0c1dfa3ec92ed5b7c860d78531b31cf2ae0535ef
HuyLe94 <70664981+HuyLe94@users.noreply.github.com>
2020-10-17 15:14:56 -0700

    Add DangerZone

---
+++ Bullet Hell/Assets/Scripts/DangerZone.cs
@@ -0,0 +1,33 @@
+﻿using Combat;
+using System.Collections;
+using System.Collections.Generic;
+using System.Runtime.ExceptionServices;
+using UnityEngine;
+using UnityEngine.Tilemaps;
+public class DangerZone : MonoBehaviour
+{
+    public Tilemap danger;
+    //GameObject player = GameObject.Find("Player_v2");
+    //Combatant playerScript = player.GetComponent<Combatant>();
+    //[SerializeField] private int health = GameObject.Find("Player_v2").GetComponent<Combatant>()._health;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.tag=="Player")
+        {
+            GameObject.Find("Player_v2").GetComponent<Combatant>()._health = 5;
+            Debug.Log("Entered");
+        }
+    }
+}
--- Bullet Hell/Assets/Scripts/RandomEnemy.cs
+++ Bullet Hell/Assets/Scripts/RandomEnemy.cs
@@ -1,43 +1,89 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Tilemaps;
+using UnityEngine.UIElements;
 
 public class RandomEnemy : MonoBehaviour
 {
     public Enemy1[] enemy;
+    public Tilemap tilemap; //= GetComponent<Tilemap>();
     public int EnemyLimit;
-     int xPos;
-     int yPos;
-     int enemyCount;
+    int xPos;
+    int yPos;
+    int enemyCount;
     public int xMapMin = -10;
     public int xMapMax = 10;
     public int yMapMin = -10;
     public int yMapMax = 10;
+ 
 
+    //private bool SpawnHere;
     // Start is called before the first frame update
     void Start()
     {
         StartCoroutine(EnemyDrop());
     }
-    IEnumerator EnemyDrop()
-    {
-        while (enemyCount < EnemyLimit)
-        {
-            xPos = Random.Range(xMapMin, xMapMax);
-            yPos = Random.Range(yMapMin, yMapMax);
 
-            // get player object
-            var player = GameObject.FindObjectOfType<PlayerMovement>();
-            
-            // set playerrb in enemy object
-            var enem = enemy[Random.Range(0, 2)];
-            enem.playerRB = player.rb;
-            
-            Instantiate(enem, new Vector3(xPos, yPos, 0), Quaternion.identity);
-            yield return new WaitForSeconds(0.1f);
-            enemyCount += 1;
+    void Update()
+    {
+
+    }
+    IEnumerator EnemyDrop()
+    {   //TESTING
+    //    foreach (var position in tilemap.cellBounds.allPositionsWithin)
+    //    //SpawnHere = false;
+    //    {
+    //        tilemap.SetColor(position, Color.yellow);
+    //        Debug.Log("Hello: " + position);
+    //    }
+
+        while (enemyCount < EnemyLimit)
+            {
+
+                xPos = Random.Range(xMapMin, xMapMax);
+                yPos = Random.Range(yMapMin, yMapMax);
+
+                // get player object
+                var player = GameObject.FindObjectOfType<PlayerMovement>();
+
+                // set playerrb in enemy object
+                var enem = enemy[Random.Range(0, 1)];
+                enem.playerRB = player.rb;
+                Vector3Int position =new Vector3Int(xPos , yPos, 0);
+            if (tilemap.HasTile(position))
+            {
+                Debug.Log("no spawn: " + position);
+            }
+            else
+            {
+                Instantiate(enem, position, Quaternion.identity);
+                yield return new WaitForSeconds(0.1f);
+                Debug.Log("NO WALL: " + position);
+                enemyCount += 1;
+            }
+            }
         }
     }
 
+//SAVING PART FOR TEST
+//public Tilemap tilemap;
+//public List<Vector3> tileWorldLocations;
 
-}
+//// Use this for initialization
+//void Start()
+//{
+//    tileWorldLocations = new List<Vector3>();
+
+//    foreach (var pos in tilemap.cellBounds.allPositionsWithin)
+//    {
+//        Vector3Int localPlace = new Vector3Int(pos.x, pos.y, pos.z);
+//        Vector3 place = tilemap.CellToWorld(localPlace);
+//        if (tilemap.HasTile(localPlace))
+//        {
+//            tileWorldLocations.Add(place);
+//        }
+//    }
+
+//    print(tileWorldLocations);
+//}
