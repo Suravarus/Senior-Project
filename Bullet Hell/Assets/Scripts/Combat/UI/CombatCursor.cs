2e398fbecd2d3862fec5db0cb309eec61430cb38
Johnny <17281817+chvz-creates@users.noreply.github.com>
2020-11-09 05:21:11 -0600

    ADDED CombatCursor
    - CombatCursor component changes
      the mouse icon based on wether the
      current mouse-position is within the
      player's weapon-range.

---
+++ Bullet Hell/Assets/Scripts/Combat/UI/CombatCursor.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using UnityEngine;
+
+namespace Combat.UI
+{
+    /// <summary>
+    /// Changes the mouse cursor based on wether it's position is within
+    /// the player's weapon-range.
+    /// <list type="bullet">
+    /// <item>This should only be added to the Player character</item>
+    /// <item>Requires PlayerMovement Component</item>
+    /// <item>Requires Combatant Component</item>
+    /// </list>
+    /// </summary>
+    [RequireComponent(typeof(PlayerMovement))]
+    [RequireComponent(typeof(Combatant))]
+    class CombatCursor : MonoBehaviour
+    {
+        // UnityEditor Properties -----------------------//
+        public Texture2D inRangeCursor;
+        public Texture2D outOfRangeCursor;
+        // ---------------------------------------------//
+
+        private Combatant playerCombatant;
+        private Boolean inRange = false;
+        void Start()
+        {
+            this.playerCombatant = this.GetComponent<Combatant>();
+            Cursor.SetCursor(this.outOfRangeCursor, Vector2.zero, CursorMode.Auto);
+        }
+        void Update()
+        {
+            var cursorPosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+            cursorPosition.z = 1;
+            if (this.playerCombatant != null)
+            {
+                if (this.playerCombatant.RangedWeapon.InRange(cursorPosition))
+                {
+                    if (!this.inRange)
+                    {
+                        this.inRange = true;
+                        Cursor.SetCursor(this.inRangeCursor, Vector2.zero, CursorMode.Auto);
+                    }
+                        
+                } else if (this.inRange)
+                {
+                    this.inRange = false;
+                    Cursor.SetCursor(this.outOfRangeCursor, Vector2.zero, CursorMode.Auto);
+                }
+            }
+        }
+    }
+}
