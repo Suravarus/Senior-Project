7061a3bab2e273fc242a791b1a2a58af0283381e
HuyLe94 <70664981+HuyLe94@users.noreply.github.com>
2020-11-10 20:10:58 -0800

    Loot Mech

---
+++ Bullet Hell/Assets/Scripts/Loot/Pickup.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Pickup : MonoBehaviour
+{
+    public List<string> inven;
+    //private Inventory inventory;
+    // Start is called before the first frame update
+    //private void Start()
+    //{
+    //    inventory = GameObject.FindGameObjectsWithTag("Player").GetComponent<Inventory>();
+    //}
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.CompareTag("Items"))
+        {
+            string itemType = other.gameObject.GetComponent<itemType>().type;
+            inven.Add(itemType);
+            Destroy(other.gameObject);
+        }
+    }
+}
---
+++ Bullet Hell/Assets/Scripts/Loot/itemType.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class itemType : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public string type;
+}
