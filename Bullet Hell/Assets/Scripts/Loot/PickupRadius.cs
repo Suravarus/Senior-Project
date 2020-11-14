72b318158e533395d92eda74f5b8c710a85a76c3
Johnny <17281817+chvz-creates@users.noreply.github.com>
2020-11-14 08:53:02 -0600

    MISC changes
    - BROKEN until otherwise stated

---
+++ Bullet Hell/Assets/Scripts/Loot/PickupRadius.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+
+namespace Loot
+{
+    [RequireComponent(typeof(CircleCollider2D))]
+    class PickupRadius : MonoBehaviour
+    {
+        private CircleCollider2D Collider2D { get; set; }
+        public void Awake()
+        {
+            if (!this.Collider2D.isTrigger)
+                this.Collider2D.isTrigger = true;
+        }
+
+        public void OnTriggerStay2D()
+        {
+            Debug.Log($"Press F to pickup {this.gameObject.name}");
+        }
+    }
+}
