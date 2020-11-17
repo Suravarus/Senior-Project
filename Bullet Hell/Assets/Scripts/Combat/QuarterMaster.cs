46da45e5af1e6402f602062a4a3f553104049f96
Johnny <17281817+chvz-creates@users.noreply.github.com>
2020-11-17 02:15:50 -0600

    Update QuarterMaster.cs
    - The PostWeaponBarStart is set as delegate WeaponBarUI.PostStart so that WeaponBarUI will call it at the end of it's Start() method.
    - This is done in case the WeaponBarUI has not yet run it's Start() method at the time that QuarterMaster is instanciated.

--- Bullet Hell/Assets/Scripts/Combat/QuarterMaster.cs
+++ Bullet Hell/Assets/Scripts/Combat/QuarterMaster.cs
@@ -82,6 +82,9 @@ namespace Combat
             {
                 this.WeaponBar = abilityBar;
                 this.WeaponBar.SetWeapons(this);
+                // SET post-start function for WeaponBar in case
+                // it still has not completed it's Start() method.
+                this.WeaponBar.PostStart = this.PostWeaponBarStart;
             }
         }
 
@@ -192,6 +195,16 @@ namespace Combat
         public Weapon[] GetArsenal()
         {
             return this.Arsenal.ToArray();
+        }
+
+        /// <summary>
+        /// This function is set in the contructor as a delegate to the 
+        /// WeaponBar.
+        /// </summary>
+        /// <param name="weaponBar">The WeaponBar that will call this method</param>
+        private void PostWeaponBarStart(WeaponBarUI weaponBar)
+        {
+            weaponBar.SetWeapons(this);
         }
     }
 }
