165753be6e9b6787830466e01415575dcc92c6b4
HuyLe94 <70664981+HuyLe94@users.noreply.github.com>
2020-11-18 14:06:06 -0800

    Moved prefabs, pickup Key implement
    pickupkey = F
    moved prefab folder to resources

--- Bullet Hell/Assets/Scripts/Loot/Pickup.cs
+++ Bullet Hell/Assets/Scripts/Loot/Pickup.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using Combat;
 using UI;
+using System.Collections;
 namespace Loot
 {
     public class Pickup : MonoBehaviour
@@ -10,50 +11,57 @@ namespace Loot
         public List<Item> inven;
         public int gold = 0;
         public CurrencyUI currencyUI;
-
+        Collider2D touch = null;
         private void Start()
         {
             if (this.currencyUI == null)
                 throw new MissingFieldException(nameof(this.currencyUI));
         }
 
-        void OnTriggerEnter2D(Collider2D other)
+        private void Update()
         {
-            if (other.CompareTag("Items"))
+            if (GetComponent<PlayerMovement>().Fkey == true && touch != null)
             {
-                
-                //Item itemType = other.gameObject.GetComponent<itemType>().type;
-                //inven.Add(itemType);
-               
-                Destroy(other.gameObject);
-            }
+                if (touch.CompareTag("Items"))
+                {
 
-            if (other.GetComponent<Weapon>() != null)
-            {
-                var weapon = other.GetComponent<Weapon>();
-                weapon.gameObject.GetComponent<CircleCollider2D>().enabled = false;
-                this.GetComponent<WeaponWielder>().GetQuarterMaster().PickupWeapon(weapon);
-            }
+                    //Item itemType = other.gameObject.GetComponent<itemType>().type;
+                    //inven.Add(itemType);
 
-            if (other.CompareTag("Regen"))
-            {
-                other.GetComponent<Combatant>().Health += 300;
-                Destroy(other.gameObject);
-            }
+                    Destroy(touch.gameObject);
+                }
 
-            if (other.CompareTag("Coins"))
-            {
-                gold += 10;
-                currencyUI.SetAmount(gold);
-                Destroy(other.gameObject);
-            }
+                if (touch.GetComponent<Weapon>() != null)
+                {
+                    var weapon = touch.GetComponent<Weapon>();
+                    weapon.gameObject.GetComponent<CircleCollider2D>().enabled = false;
+                    this.GetComponent<WeaponWielder>().GetQuarterMaster().PickupWeapon(weapon);
+                }
 
-            //if (other.CompareTag("Ammo"))
-            //{
-            //     += 10;
-            //    Destroy(other.gameObject);
-            //}
+                if (touch.CompareTag("Regen"))
+                {
+                    touch.GetComponent<Combatant>().Health += 300;
+                    Destroy(touch.gameObject);
+                }
+
+                if (touch.CompareTag("Coins"))
+                {
+                    gold += 10;
+                    currencyUI.SetAmount(gold);
+                    Destroy(touch.gameObject);
+                }
+
+                //if (other.CompareTag("Ammo"))
+                //{
+                //     += 10;
+                //    Destroy(other.gameObject);
+                //}
+            }
         }
-       
+        void OnTriggerStay2D(Collider2D other)
+        {
+            touch = other;
+        }
+        
     }
 }
