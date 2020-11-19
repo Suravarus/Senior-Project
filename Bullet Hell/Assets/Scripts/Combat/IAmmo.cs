cdbd5ff15c8a34eece15edea1ce1cdf39ee255fe
Johnny <17281817+chvz-creates@users.noreply.github.com>
2020-11-18 18:12:50 -0600

    Update IAmmo.cs
    - updated documentation

--- Bullet Hell/Assets/Scripts/Combat/IAmmo.cs
+++ Bullet Hell/Assets/Scripts/Combat/IAmmo.cs
@@ -1,4 +1,5 @@
-﻿using Utilities;
+﻿using System;
+using Utilities;
 using UnityEngine;
 
 namespace Combat
@@ -10,12 +11,30 @@ namespace Combat
         /// to the user via UI.
         /// </summary>
         GameInfo Info { get; set; }
+        /// <summary>
+        /// The price of this item.
+        /// </summary>
+        [Obsolete("This will be moved into the GameInfo class.")]
         int Price { get; set; }
+        /// <summary>
+        /// The speed at which this bullet should travel.
+        /// </summary>
         float Speed { get; }
+        /// <summary>
+        /// The added damage that this bullet causes.
+        /// </summary>
         int Damage { get; set; }
+        /// <summary>
+        /// The weapon that from which the bullet was fired.
+        /// </summary>
         Weapon Weapon { get; set; }
+        /// <summary>
+        /// The WeaponWielder that fired this bullet.
+        /// </summary>
         WeaponWielder Shooter { get; set; }
-        
+        /// <summary>
+        /// The position at which this bullet spawned.
+        /// </summary>
         Vector3 StartingPosition { get; set; }
     }
 }
