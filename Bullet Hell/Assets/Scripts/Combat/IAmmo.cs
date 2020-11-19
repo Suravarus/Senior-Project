493cf3131abb86c3a6470d488a9b3f5670fc664f
Johnny <17281817+chvz-creates@users.noreply.github.com>
2020-11-18 18:08:40 -0600

    Varia Integration I
    - CREATED IAmmo and IWeapon interfaces to determine what these objects must have in order to maintain functionality with existing systems.
    - I will use these to determine what code is no longer neccessary with Varia.

---
+++ Bullet Hell/Assets/Scripts/Combat/IAmmo.cs
@@ -0,0 +1,21 @@
+﻿using Utilities;
+using UnityEngine;
+
+namespace Combat
+{
+    public interface IAmmo
+    {
+        /// <summary>
+        /// Data about this item that will be used displayed
+        /// to the user via UI.
+        /// </summary>
+        GameInfo Info { get; set; }
+        int Price { get; set; }
+        float Speed { get; }
+        int Damage { get; set; }
+        Weapon Weapon { get; set; }
+        WeaponWielder Shooter { get; set; }
+        
+        Vector3 StartingPosition { get; set; }
+    }
+}
---
+++ Bullet Hell/Assets/Scripts/Combat/IWeapon.cs
@@ -0,0 +1,69 @@
+﻿using UI;
+using Utilities;
+using UnityEngine;
+
+namespace Combat
+{
+    public interface IWeapon
+    {
+        /// <summary>
+        /// Information such as weapon name, and description.
+        /// This data will be displayed to the user via UI.
+        /// </summary>
+        GameInfo Info { get; }
+        /// <summary>
+        /// If set, this weapon will update this UISlot with 
+        /// it's current AmmoCount.
+        /// </summary>
+        Slot UIAmmoSlot { get; set; }
+        /// <summary>
+        /// The damage that this weapon does regardless of Ammo
+        /// </summary>
+        float BaseDamage { get; set; }
+        /// <summary>
+        /// The distance that the bullets fired from this weapon
+        /// can travel before they are destroyed.
+        /// </summary>
+        float Range { get; set; }
+        /// <summary>
+        /// IF TRUE, the weapon will shoot regardless of the 
+        /// AmmoCount.
+        /// </summary>
+        bool InfiniteAmmo { get; set; }
+        /// <summary>
+        /// The amount of ammo available for this
+        /// weapon to use.
+        /// </summary>
+        int AmmoCount { get; set; }
+        /// <summary>
+        /// The ammo prefab that this weapon is to fire.
+        /// </summary>
+        IAmmo WeaponAmmo { get; set; }
+        Animator ShootingAnimator { get; set; }
+        /// <summary>
+        /// The WeaponWielder that is currently using 
+        /// this weapon.
+        /// </summary>
+        WeaponWielder Wielder { get; set; }
+
+        /// <summary>
+        /// Flips the weapon on the X-Axis
+        /// </summary>
+        void Flip();
+        /// <summary>
+        /// Returns TRUE if this weapon is currently flipped.
+        /// </summary>
+        /// <returns></returns>
+        bool IsFlipped();
+        /// <summary>
+        /// Returns true if the target vector is within weapon range.
+        /// </summary>
+        /// <param name="target">Worldspace point</param>
+        /// <returns></returns>
+        bool InRange(Vector3 target);
+        /// <summary>
+        /// Shoots the weapon only if all contraints are met.
+        /// </summary>
+        void RequestWeaponFire();
+    }
+}
