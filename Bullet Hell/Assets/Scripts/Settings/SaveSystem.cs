6843797acbc018fe78c51710b8a85eddfff9fca5
jpilola <jdpilola@gmial.com>
2020-11-21 13:48:05 -0600

    Save System
    Need to make it function with the pause menu

---
+++ Bullet Hell/Assets/Scripts/Settings/SaveSystem.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+using System.IO;
+using System.Runtime.Serialization.Formatters.Binary;
+using Combat;
+
+public class SaveSystem 
+{
+    public static void SavePlayer (WeaponWielder player)
+    {
+        BinaryFormatter formatter = new BinaryFormatter();
+        string path = Application.persistentDataPath + "/player.txt";
+        FileStream stream = new FileStream(path, FileMode.Create);
+
+        PlayerData data = new PlayerData(player);
+        formatter.Serialize(stream, data);
+        stream.Close();
+    }
+
+    public static PlayerData LoadPlayer ()
+    {
+        string path = Application.persistentDataPath + "/player.txt";
+        if (File.Exists(path))
+        {
+            BinaryFormatter formatter = new BinaryFormatter();
+            FileStream stream = new FileStream(path, FileMode.Open);
+
+            PlayerData data = formatter.Deserialize(stream) as PlayerData;
+            stream.Close();
+
+            return data;
+        }
+        else
+        {
+            Debug.LogError("Save File not found in " + path);
+            return null;
+        }
+    }
+}
